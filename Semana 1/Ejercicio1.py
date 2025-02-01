ce4eebf92c48bb6e2b7b62d64be7f296f4580939
MSI SteelSeries <roel.guzmanm@uanl.edu.mx>
2025-02-01 15:21:00 -0600

    ejercicio 1 y 2

---
+++ Semana 1/Ejercicio1.py
@@ -0,0 +1,9 @@
+numA = int(input("Inserte el primer número: "))
+numB = int(input("Inserte el segundo número: "))
+
+print("Suma", numA+numB)
+print("Resta", numA-numB)
+print("Multiplicación", numA*numB)
+print("División", numA/numB)
+print("Modulo", numA//numB)
+print("Residuo", numA%numB)
---
+++ Semana 1/Ejercicio2.py
@@ -0,0 +1,8 @@
+print("Ejercicio 2: ¿Cuál es el número mas grande?")
+num1 = float(input("Inserte el primer numero: "))
+num2 = float(input("Inserte el segundo numero: "))
+
+if (num1 > num2):
+    print(f"{num1} Es mayor que {num2}")
+else:
+    print(f"{num2} Es mayor que {num1}")
