c3852aecff1c078f7fc64f1011006e2b60448c77
MSI SteelSeries <roel.guzmanm@uanl.edu.mx>
2025-02-01 15:48:43 -0600

    Ejercicio 6

---
+++ Semana 1/Ejercicio6.py
@@ -0,0 +1,6 @@
+print("Ejercicio 6: Convierte grados Celcius a grados Fahrenheit y Kelvin")
+grad = float(input("Ingresa los grados Celsius a convertir: "))
+fah = (1.8*grad) + 32
+kelvin = grad + 273.15
+
+print(f"{grad}° Celcius son {fah}° Fahrenheit y {kelvin}° Kelvin")
