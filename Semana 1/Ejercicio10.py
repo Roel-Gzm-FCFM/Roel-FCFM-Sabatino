7a00a08f9ac90b36b246e411e52e9130ffb9a5db
MSI SteelSeries <roel.guzmanm@uanl.edu.mx>
2025-02-01 19:26:51 -0600

    Ejercicio 10

---
+++ Semana 1/Ejercicio10.py
@@ -0,0 +1,8 @@
+print("Ejercicio 10: ¿Cúal es el área del triángulo?")
+
+base = float(input("Ingresa la longitud de la base: "))
+altura = float(input("Ingresa la longitud de la altura: "))
+
+area = (base*altura)/2
+
+print(f"El área del triángulo es {area} unidades cuadradas.")
