8cf6e724aaf45b14bf9d0710accc9fdc66a07943
MSI SteelSeries <roel.guzmanm@uanl.edu.mx>
2025-02-01 19:21:19 -0600

    Ejercicio 9

---
+++ Semana 1/Ejercicio9.py
@@ -0,0 +1,9 @@
+print("Ejercicio 9: ¿Cúal es el promedio?")
+
+c1 = float(input("Ingresa la 1er calificación: "))
+c2 = float(input("Ingresa la 2da calificación: "))
+c3 = float(input("Ingresa la 3era calificación: "))
+
+prom = (c1 + c2 + c3)/3
+
+print(f"El promedio de las calificaciones es: {prom}.")
