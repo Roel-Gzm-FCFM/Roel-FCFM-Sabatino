ae64e35892c92b421b98144d7e6b3b675e96d240
MSI SteelSeries <roel.guzmanm@uanl.edu.mx>
2025-02-08 15:31:04 -0600

    Actividad 3

---
+++ Semana 2/Actividad3.py
@@ -0,0 +1,6 @@
+num = int(input("Ingresa un número: "))
+
+if num%2 != 0 and num/num == 1 and num/1:
+    print(f"El número {num} es un número primo.")
+else:
+    print(f"El número {num} no es un número primo.")
